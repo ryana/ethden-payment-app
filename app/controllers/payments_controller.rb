7fb5f0707eb2e61bea1e152c424b9e7d2428bd68
Ryan Angilly <ryan@angilly.com>
2018-02-18 08:37:04 -0700

    finish wiring up demo payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,7 @@ class PaymentsController < ApplicationController
   def create
     session[:payment] = {
       'id' => SecureRandom.uuid,
-      'amount' => params[:amount],
+      'amount' => params[:amount].to_f,
       'address' => params[:address],
       'redirect_url' => params[:redirect_url]
     }
@@ -21,8 +21,35 @@ class PaymentsController < ApplicationController
       session[:payment]['verified'] = true
     end
 
+    # The things we do on no sleep! :-D
+    url = Rails.env.production? ?
+      "https://quicketh-rinkeby.herokuapp.com/mint" :
+      "http://localhost:5000/mint"
+
     if params[:stripe_token]
-      redirect_to session[:payment]['redirect_url']
+      addr = session[:payment]['address']
+      amount = session[:payment]['amount']
+
+      resp = Typhoeus::Request.post(url, body: {
+        destinationAddress: addr,
+        amount: amount
+      }.to_json,
+      headers: {
+        'Content-Type' => 'application/json'
+      });
+
+      txid = JSON.parse(resp.body)['transactionId']
+
+      rurl = session[:payment]['redirect_url']
+      if rurl.include? "?"
+        rurl << "&"
+      else
+        rurl << "?"
+      end
+
+      rurl << "quickethTxId=#{txid}"
+      redirect_to rurl
+
       session[:payment] = nil
     else
       redirect_to payment_path(session[:payment]['id'])
